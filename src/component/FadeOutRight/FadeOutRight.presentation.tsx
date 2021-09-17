87acb68e1a7055eb90133c1f3917bcf13b82ad13
sochetra NOV <sochetra12.nov@gmail.com>
2021-09-17 09:25:36 +0700

    add: FadeOutRight component

---
+++ src/component/FadeOutRight/FadeOutRight.presentation.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { ViewStyle } from 'react-native';
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
+import { IFadeOutRightPresentationProps, Constant } from './';
+
+export const FadeOutRightPresentation: React.FC<IFadeOutRightPresentationProps> =
+  (props) => {
+    const opacity = useSharedValue(Constant.MAX_OPACITY);
+    const translateX = useSharedValue(props.startPostionX);
+
+    const animatedStyle = useAnimatedStyle<
+      Reanimated.AnimatedStyleProp<ViewStyle>
+    >(() => {
+      return {
+        opacity: opacity.value,
+        transform: [{ translateX: translateX.value }],
+      };
+    });
+
+    useEffect(() => {
+      opacity.value = withDelay(
+        props.delayInMS,
+        withTiming(
+          Constant.MIN_OPACITY,
+          {
+            duration: Constant.DEFAULT_DURATION_IN_MS,
+          },
+          // TODO: Refactor this function
+          callbackFinishOpacity,
+        ),
+      );
+
+      translateX.value = withDelay(
+        props.delayInMS,
+        withTiming(
+          Constant.FINAL_POSITION_X,
+          {
+            duration: Constant.DEFAULT_DURATION_IN_MS,
+          },
+          callbackFinishTranslateX,
+        ),
+      );
+    }, []);
+
+    const callbackFinishOpacity = (isFinished: boolean): void => {
+      if (isFinished) {
+        opacity.value = Constant.MAX_OPACITY;
+      }
+    };
+
+    const callbackFinishTranslateX = (isFinished: boolean): void => {
+      if (isFinished) {
+        translateX.value = Constant.INITIAL_POSITION_X;
+      }
+    };
+
+    return (
+      <Reanimated.View style={animatedStyle}>{props.children}</Reanimated.View>
+    );
+  };
---
+++ src/component/FadeOutRight/index.ts
@@ -0,0 +1,4 @@
+export * from './FadeOutRight.container';
+export * from './FadeOutRight.interfaces';
+export * from './FadeOutRight.presentation';
+export * from './FadeOutRight.constant';
