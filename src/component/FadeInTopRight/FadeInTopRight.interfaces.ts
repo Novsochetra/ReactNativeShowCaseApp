0a993763558be483bea9ca9b3c9262b624a67f9d
sochetra NOV <sochetra12.nov@gmail.com>
2021-09-17 08:09:40 +0700

    add: FadeInTopRight component

---
+++ src/component/FadeInTopRight/FadeInTopRight.interfaces.ts
@@ -0,0 +1,13 @@
+export interface IFadeInTopRightContainerProps {
+  delayInMS?: number;
+  durationInMS?: number;
+  startPostionY?: number;
+  startPostionX?: number;
+}
+
+export interface IFadeInTopRightPresentationProps {
+  delayInMS: number;
+  durationInMS: number;
+  startPostionY: number;
+  startPostionX: number;
+}
