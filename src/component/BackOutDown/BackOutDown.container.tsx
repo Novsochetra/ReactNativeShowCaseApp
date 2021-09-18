9ac40d491e445397777c42451b6276c0d030107c
sochetra NOV <sochetra12.nov@gmail.com>
2021-09-18 12:12:31 +0700

    add: BackOutDown component

---
+++ src/component/BackOutDown/BackOutDown.container.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { ViewStyle } from 'react-native';
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
+import {
+  IBackOutDownContainerProps,
+  BackOutDownPresentation,
+  Constant,
+} from '.';
+
+export const BackOutDown: React.FC<IBackOutDownContainerProps> = ({
+  children,
+  delayInMS = Constant.DEFAULT_DELAY_IN_MS,
+  durationInMS = Constant.DEFAULT_DURATION_IN_MS,
+  initialPositionY = Constant.INITIAL_POSITION_Y,
+}) => {
+  const opacity = useSharedValue(Constant.MAX_OPACITY);
+  const translateY = useSharedValue(initialPositionY);
+  const scale = useSharedValue(Constant.MAX_SCALE);
+
+  const animatedStyle = useAnimatedStyle<Reanimated.AnimateStyle<ViewStyle>>(
+    () => {
+      return {
+        opacity: opacity.value,
+        transform: [{ translateY: translateY.value }, { scale: scale.value }],
+      };
+    },
+  );
+
+  useEffect(() => {
+    onAnimateOpacity();
+    onAnimateScale();
+    onAnimateTranslateY();
+  }, []);
+
+  const onAnimateScale = () => {
+    scale.value = withDelay(
+      delayInMS,
+      withTiming(Constant.MIN_SCALE, {
+        duration: durationInMS,
+      }),
+    );
+  };
+
+  const onAnimateTranslateY = () => {
+    const translateYDelayDuration = delayInMS + durationInMS;
+    translateY.value = withDelay(
+      translateYDelayDuration,
+      withTiming(
+        Constant.FINAL_POSITION_Y,
+        {
+          duration: durationInMS,
+        },
+        onFinishAnimateTranslateY,
+      ),
+    );
+  };
+
+  const onFinishAnimateTranslateY = (isFinished: boolean) => {
+    'worklet';
+    if (isFinished) {
+      translateY.value = initialPositionY;
+      opacity.value = Constant.MAX_OPACITY;
+      scale.value = Constant.MAX_SCALE;
+    }
+  };
+
+  const onAnimateOpacity = () => {
+    const opacityDuration = delayInMS + durationInMS;
+    opacity.value = withDelay(
+      delayInMS,
+      withTiming(Constant.MIN_OPACITY, {
+        duration: opacityDuration,
+      }),
+    );
+  };
+
+  return (
+    <BackOutDownPresentation containerStyle={animatedStyle}>
+      {children}
+    </BackOutDownPresentation>
+  );
+};
